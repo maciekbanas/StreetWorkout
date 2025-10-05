03d851ec021b54ededf0bdb04700462e08f65fbe
Maciej Banas <banasmaciek@gmail.com>
2025-10-05 22:15:07 +0200

    Remove name of version from title.

--- R/workout_app.R
+++ R/workout_app.R
@@ -9,7 +9,7 @@ workout_app <- function() {
     title = "Workout App",
     shinyMobile::f7TabLayout(
       navbar = shinyMobile::f7Navbar(
-        title = paste0("Workout App ", print_test_mode(), " (", app_version, ") ")
+        title = paste0("Street Workout App ", print_test_mode())
       ),
       shinyMobile::f7Tabs(
         id = "tabs",
